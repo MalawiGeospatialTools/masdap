4a5f5333da9defde299c711831fb18495bc525c5
sbsimo <simone.balbo@ithaca.polito.it>
2018-08-28 10:31:23 +0200

    cleaning step 1

--- account_captcha/forms.py
+++
@@ -1,6 +0,0 @@
-from nocaptcha_recaptcha.fields import NoReCaptchaField
-
-from account.forms import SignupForm
-
-class ReCaptchaSignupForm(SignupForm):
-    captcha = NoReCaptchaField()
--- account_captcha/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from account_captcha.views import SignupViewCaptcha
-
-urlpatterns = patterns('',
-    url(r'^$', SignupViewCaptcha.as_view(), name='account_signup'),
-)
