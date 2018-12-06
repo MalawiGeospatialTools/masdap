be0819f304ae1f37a68ab449124e5706da265fac
sbsimo <simone.balbo@ithaca.polito.it>
2018-12-06 16:08:12 +0100

    add structure for local settings file

---
+++ masdap/local_settings.py
@@ -0,0 +1,3 @@
+NORECAPTCHA_SITE_KEY = 'insert here your site key'
+NORECAPTCHA_SECRET_KEY = 'insert here your secret key'
+# NORECAPTCHA_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
