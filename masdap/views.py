dd50b4689f411e435181a952cf046f98cfafc108
Paolo Pasquali <paolo.pasquali@gmail.com>
2018-12-19 12:21:05 +0100

    Add contact success message

--- masdap/views.py
+++ masdap/views.py
@@ -1,7 +1,8 @@
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -13,9 +14,13 @@ def contact(request):
             sender_email = form.cleaned_data['email']
 
             message = "{0} has sent you a new message:\n\n{1}".format(sender_name, form.cleaned_data['message'])
-            send_mail('New Enquiry', message, sender_email, ['masdap.mw@gmail.com'])
-            return HttpResponse('Thanks for contacting us!')
+            send_mail('New contact form submission on MASDAP', message, sender_email, ['masdap.mw@gmail.com'])
+            #return HttpResponse('Thanks for contacting us!')
+            messages.success(request, 'Thanks for reaching out! Your message has been sent.')
+            return redirect('contact')
     else:
         form = ContactForm()
 
-    return render(request, 'contact.html', {'form': form})
+    return render(request, 'contact.html', {
+        'form': form
+    })
