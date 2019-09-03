58acce3d06f39b6be3d07c3eccb5c7b994940c54
ppasq <paolo.pasquali@gmail.com>
2019-09-03 09:40:35 +0200

    Add django-recaptcha

--- masdap/forms.py
+++ masdap/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext as _
 
+from captcha.fields import ReCaptchaField
 
 class ContactForm(forms.Form):
 	name = forms.CharField(max_length=100)
 	email = forms.EmailField()
 	message = forms.CharField(widget=forms.Textarea)
+	captcha = ReCaptchaField()
 
 	def clean(self):
 		cleaned_data = super(ContactForm, self).clean()
@@ -14,4 +16,13 @@ class ContactForm(forms.Form):
 		email = cleaned_data.get('email')
 		message = cleaned_data.get('message')
 		if not name and not email and not message:
-			raise forms.ValidationError('You have to write somenthing!')
+			raise forms.ValidationError('You should write something.')
+
+
+class AllauthSignupForm(forms.Form):
+
+	captcha = ReCaptchaField()
+
+	def signup(self, request, user):
+		""" Required, or else it thorws deprecation warnings """
+		pass
